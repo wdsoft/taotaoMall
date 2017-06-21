c5231bcff2c0ad8a734543e3638c1a8e4048b51e
jiwendong <jiwendong09@163.com>
2017-06-21 23:42:41 +0800

    Initial commit

---
+++ taotao-manager/taotao-manager-service/src/main/java/net/wdsoft/taotao/App.java
@@ -0,0 +1,13 @@
+package net.wdsoft.taotao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
